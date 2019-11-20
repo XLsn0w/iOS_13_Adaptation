620dce85b099ee8e676387cd23469e26fe12cfef
XLsn0w <xlsn0wios@gmail.com>
2019-11-20 10:06:36 +0800

    add

---
+++ iOS_13_Adaptation/iOS13Adaptation.m
@@ -0,0 +1,13 @@
+//
+//  iOS13Adaptation.m
+//  iOS_13_Adaptation
+//
+//  Created by mac on 2019/11/20.
+//  Copyright © 2019 XLsn0w. All rights reserved.
+//
+
+#import "iOS13Adaptation.h"
+
+@implementation iOS13Adaptation
+
+@end
